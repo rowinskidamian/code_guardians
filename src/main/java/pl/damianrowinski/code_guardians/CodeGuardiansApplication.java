c7a79cfd283d29c016aca13fb97e4a003a80fc6e
Damian Rowiński <damian.rowinski@gmail.com>
2020-11-03 11:15:51 +0100

    initial commit

---
+++ src/main/java/pl/damianrowinski/code_guardians/CodeGuardiansApplication.java
@@ -0,0 +1,13 @@
+package pl.damianrowinski.code_guardians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGuardiansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGuardiansApplication.class, args);
+    }
+
+}
